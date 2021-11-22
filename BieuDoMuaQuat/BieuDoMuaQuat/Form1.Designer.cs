939abd2e399a97e9b9a2d330220f96e7707998c1
hoangkhoi8868 <phamhoangkhoi745@gmail.com>
2021-11-22 23:13:03 +0700

    xamloz

---
+++ BieuDoMuaQuat/BieuDoMuaQuat/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace BieuDoMuaQuat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.qLTVDataSet = new BieuDoMuaQuat.QLTVDataSet();
+            this.dOCGIABindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dOC_GIATableAdapter = new BieuDoMuaQuat.QLTVDataSetTableAdapters.DOC_GIATableAdapter();
+            this.dOCGIABindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.qLTVDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dOCGIABindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dOCGIABindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // qLTVDataSet
+            // 
+            this.qLTVDataSet.DataSetName = "QLTVDataSet";
+            this.qLTVDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dOCGIABindingSource
+            // 
+            this.dOCGIABindingSource.DataMember = "DOC_GIA";
+            this.dOCGIABindingSource.DataSource = this.qLTVDataSet;
+            // 
+            // dOC_GIATableAdapter
+            // 
+            this.dOC_GIATableAdapter.ClearBeforeFill = true;
+            // 
+            // dOCGIABindingSource1
+            // 
+            this.dOCGIABindingSource1.DataMember = "DOC_GIA";
+            this.dOCGIABindingSource1.DataSource = this.qLTVDataSet;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1054, 468);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.qLTVDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dOCGIABindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dOCGIABindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private QLTVDataSetTableAdapters.DOC_GIATableAdapter dOC_GIATableAdapter;
+        private QLTVDataSet qLTVDataSet;
+        private System.Windows.Forms.BindingSource dOCGIABindingSource;
+        private System.Windows.Forms.BindingSource dOCGIABindingSource1;
+    }
+}
+
---
+++ BieuDoMuaQuat/BieuDoMuaQuat/Form1.cs
@@ -0,0 +1,96 @@
+﻿using DevExpress.XtraCharts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BieuDoMuaQuat
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataTable ThongKe()
+        {
+            DataTable dt = new DataTable();
+           // dt.Columns.Add("ThuTu", typeof(String)); // số thứ tự
+            dt.Columns.Add("Sach", typeof(String));
+            dt.Columns.Add("Tong", typeof(String)); // tổng theo select 
+            /// add csdl giả
+            /// 
+            // data row thông kê
+            DataRow dr = null;
+            List<string> list_sach = new List<string>();
+            list_sach.Add("Sách hư");
+            list_sach.Add("Sách đã trả");
+            list_sach.Add("Sách đng mượn");
+            int count = 0;
+            Random rand = new Random();
+            foreach (string i in list_sach)
+            {
+                int tong = rand.Next(100, 10000);
+                 //   count++;
+                    dr = dt.NewRow();
+                //    dr["ThuTu"] = count.ToString() ;
+                    dr["Sach"] = i ;
+                    dr["Tong"] = tong.ToString() ;
+                dt.Rows.Add(dr);
+            }
+            return dt;
+            
+        }
+        // tạo biểu đồ
+        void TaoBieuDo()
+        {
+            ChartControl ThongKe1 = new ChartControl();
+            ThongKe1.Titles.Add( new DevExpress.XtraCharts.ChartTitle() { Text =  "Thống kê sách" });
+
+            Series se1 = new Series("Title thôi",ViewType.Pie); //
+           foreach(DataRow dr in ThongKe().Rows)
+            {
+                se1.Points.Add(new SeriesPoint(dr["Sach"], dr["Tong"]));
+               // MessageBox.Show(dr["Sach"].ToString());
+            }
+            se1.Label.TextPattern = "{A}: {VP:p2}";
+            se1.LegendTextPattern = "{A}";
+            ((PieSeriesLabel)se1.Label).Position = PieSeriesLabelPosition.TwoColumns;
+            ((PieSeriesLabel)se1.Label).ResolveOverlappingMode = ResolveOverlappingMode.Default;
+            // view
+            PieSeriesView view = (PieSeriesView)se1.View;
+            // Data srouce
+            se1.ArgumentDataMember = "Sach";
+            se1.ValueDataMembers.AddRange(new string[] { "Tong" });
+            //document của dev
+            ThongKe1.Series.Add(se1);
+            // Specify a data filter to explode points.
+            view.ExplodedPointsFilters.Add(new SeriesPointFilter(SeriesPointKey.Value_2,
+                DataFilterCondition.GreaterThanOrEqual, 9));
+            view.ExplodedPointsFilters.Add(new SeriesPointFilter(SeriesPointKey.Argument,
+                DataFilterCondition.NotEqual, "Others"));
+            view.ExplodeMode = PieExplodeMode.UseFilters;
+            view.ExplodedDistancePercentage = 5;
+            view.RuntimeExploding = true;
+
+            // Customize the legend.
+            ThongKe1.Legend.Visibility = DevExpress.Utils.DefaultBoolean.True;
+
+            // Add the chart to the form.
+            ThongKe1.Dock = DockStyle.Fill;
+            this.Controls.Add(ThongKe1);
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'qLTVDataSet.DOC_GIA' table. You can move, or remove it, as needed.
+            //this.dOC_GIATableAdapter.Fill(this.qLTVDataSet.DOC_GIA);
+            // gridControl1.DataSource = ThongKe();
+            TaoBieuDo();
+        }
+    }
+}
